0088458d929abb2c54233ab7dfdde0f93933116d
haresh-r2103 <haresh.r2103@gmail.com>
2024-08-30 07:16:45 +0530

    Time: 95 ms (14.04%), Space: 92.5 MB (40.49%) - LeetHub

---
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        vector<vector<int>>v(m, vector<int>(n));
+        if(n*m != original.size()){
+            return {};
+        }
+        int k=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                v[i][j] = original[k];
+                k++;
+            }
+        }
+        return v;
+    }
+};
