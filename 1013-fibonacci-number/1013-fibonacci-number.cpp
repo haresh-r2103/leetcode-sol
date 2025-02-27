0e6b79858fd901e8780fdfe0913e2c1b78e9e81c
Haresh R <haresh.r2103@gmail.com>
2025-02-27 21:15:46 +0530

    Time: 3 ms (68.01%), Space: 8.4 MB (9.72%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int fib(int n) {
         vector<int> dp(n+1, -1);
-        return findFib(dp, n);
-    }
-    int findFib(vector<int>& dp, int n){
         if(n <= 1) return n;
-
-        if(dp[n] != -1) return dp[n];
-
-        return dp[n] = findFib(dp, n-1) + findFib(dp, n-2); 
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
+    
 };
