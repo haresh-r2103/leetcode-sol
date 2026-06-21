9956d0e3bb67ab24dfd98adf72b32575d4f1b76f
Haresh R <haresh.r2103@gmail.com>
2026-06-21 20:08:26 +0530

    Time: 4 ms (19.26%), Space: 16.2 MB (35.03%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -2,26 +2,27 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n+2, vector<int>(2, -1));
-        return solve(0, 1, prices, dp);
-    }
+        vector<vector<int>> dp(n+2, vector<int>(2, 0));
 
-    int solve(int ind, int buy, vector<int>& prices, vector<vector<int>>& dp){
-        // base case
-        if(ind >= prices.size()) return 0;
-        if(dp[ind][buy] != -1) return dp[ind][buy];
-        int prof = 0;
+        // base case -> Handled while initiating the dp iteself
+        
+        for(int ind = n-1; ind >= 0; --ind){
+            for(int buy = 0; buy <= 1; ++buy){
+                int prof = 0;
 
-        if(buy){
-            int bs = -prices[ind] + solve(ind+1, 0, prices, dp);
-            int move = 0 + solve(ind+1, 1, prices, dp);
-            prof = max(bs, move);
-        } else {
-            int ss = prices[ind] + solve(ind+2, 1, prices, dp);
-            int move = 0 + solve(ind+1, 0, prices, dp);
-            prof = max(ss, move);
+                if(buy){
+                    int bs = -prices[ind] + dp[ind+1][0];
+                    int move = 0 + dp[ind+1][1];
+                    prof = max(bs, move);
+                } else {
+                    int ss = prices[ind] + dp[ind+2][1];
+                    int move = 0 + dp[ind+1][0];
+                    prof = max(ss, move);
+                }
+                dp[ind][buy] = prof;
+            }
         }
 
-        return dp[ind][buy] = prof;
+        return dp[0][1];
     }
 };
